4528f73f1e8b1d75c4688738418b74cb2da6dee5
Tina-Serria <tinaserria@gmail.com>
2023-05-15 09:30:28 +0800

    init project

---
+++ src/main/java/com/youphye/usercenter/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.youphye.usercenter.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.youphye.usercenter.pojo.User;
+import com.youphye.usercenter.service.UserService;
+import com.youphye.usercenter.mapper.UserMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author Tina Serria
+* @description 针对表【user】的数据库操作Service实现
+* @createDate 2023-05-15 08:52:45
+*/
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User>
+    implements UserService {
+
+}
+
+
+
+
